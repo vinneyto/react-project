07a2b176bef1e0dadf99e97a422ccf60df664f12
lvinnichenko <lvinnichenko@aligntech.com>
2025-03-20 18:04:33 +0100

    moves scroll logic to hook

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,27 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import styles from './ProgressBar.module.css';
+import { useProgress } from './useProgress';
 
 export const ProgressBar: React.FC = () => {
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop =
-        document.documentElement.scrollTop || document.body.scrollTop;
-      const scrollHeight =
-        document.documentElement.scrollHeight || document.body.scrollHeight;
-      const clientHeight =
-        document.documentElement.clientHeight || window.innerHeight;
-      const totalScroll = (scrollTop / (scrollHeight - clientHeight)) * 100;
-      setProgress(totalScroll);
-    };
-
-    document.addEventListener('scroll', handleScroll);
-    return () => {
-      document.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const progress = useProgress();
 
   return (
     <div className={styles.scrollProgressBarContainer}>
