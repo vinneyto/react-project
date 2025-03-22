cfd24243652e933d35dd64839838a43de1f174d1
vinneyto <vinneyto@gmail.com>
2025-03-22 21:16:35 +0100

    adds theme toggle button

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
 
+import { useTheme } from '../../hooks';
 import styles from './Header.module.css';
 
 export const Header: React.FC = () => {
+  const { toggleTheme } = useTheme();
+
   return (
     <header className={styles.header}>
       <div className={styles.content}>
         <h1>Restaurant Aggregator</h1>
-        <nav>
-          <ul className={styles.navList}>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#menu">Menu</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </nav>
+        <div className={styles.buttonContainer}>
+          <button className={styles.toggleButton} onClick={toggleTheme}>
+            T
+          </button>
+          <button className={styles.toggleButton} onClick={toggleTheme}>
+            U
+          </button>
+        </div>
       </div>
     </header>
   );
